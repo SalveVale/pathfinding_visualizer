a77ee7e739d2c28ac6c79358e7b1550105f07134
Ben Powell <benpowell751@yahoo.com>
2022-10-11 22:51:41 -0600

    generating grid, placing walls on grid, and initializing state machine

---
+++ tile.hpp
@@ -0,0 +1,63 @@
+#include <SFML/Graphics.hpp>
+
+const int BOX_SIZE = 50;
+const int PADDING =  500;
+
+class Tile {
+public:
+  Tile(int x, int y) {
+    this->box.setSize(sf::Vector2f(BOX_SIZE, BOX_SIZE));
+    this->box.setPosition(sf::Vector2f(PADDING + x*BOX_SIZE, y*BOX_SIZE));
+    this->box.setOutlineThickness(1);
+    this->box.setOutlineColor(sf::Color::Black);
+  }
+  ~Tile();
+  
+  enum states {
+    empty,
+    hovered,
+    wall,
+    start,
+    end,
+    visited,
+    path
+  } state = empty;
+  
+  sf::RectangleShape getBox() { return this->box; }
+  
+  void setState(states newState) {
+    this->state = newState;
+    
+    switch (this->state)
+    {
+      case empty:
+        this->box.setFillColor(sf::Color(155, 155, 155, 255));
+        break;
+      case hovered:
+        this->box.setFillColor(sf::Color(55, 55, 55, 255));
+        break;
+      case wall:
+        this->box.setFillColor(sf::Color(12, 12, 12, 255));
+        break;
+      case start:
+        this->box.setFillColor(sf::Color::Green);
+        break;
+      case end:
+        this->box.setFillColor(sf::Color::Red);
+        break;
+      case visited:
+        this->box.setFillColor(sf::Color::Blue);
+        break;
+      case path:
+        this->box.setFillColor(sf::Color::Yellow);
+        break;
+    }
+  }
+  
+  bool containsMouse(sf::Vector2f mousePos) { return this->box.getGlobalBounds().contains(mousePos); }
+
+private:
+  sf::RectangleShape box;
+  // int index;
+  // Tile *prevTile;
+};
--- window.hpp
+++ window.hpp
@@ -2,6 +2,23 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
+#include "tile.hpp"
+
+class StateEngine {
+public:
+  StateEngine();
+  ~StateEngine();
+
+  enum states {
+    welcome,
+    build,
+    solve
+  } state = welcome;
+
+  void setState(states newState) { this->state = newState; }
+};
+
+
 class Window {
 public:
   Window() {
@@ -10,6 +27,15 @@ public:
   }
   ~Window() {
     delete this->window;
+    
+    // for (int i=0; i<20; i++)
+    // {
+    //   for (int j=0; j<20; j++)
+    //   {
+    //     delete this->grid[i][j];
+    //     this->grid[i][j] = 0;
+    //   }
+    // }
   }
 
   const bool isOpen() const {
@@ -18,28 +44,48 @@ public:
 
   void update() {
     this->pollEvents();
+    this->updateMouse();
+    this->updateTiles();
   }
   
   void render() {
-    this->window->clear(sf::Color::White);
+    this->window->clear();
+    this->renderBoxes();
     this->window->display();
   }
   
 private:
+  //variables
   sf::RenderWindow *window;
   sf::VideoMode videoMode;
   sf::Event event;
+  Tile *grid[20][20];
+  
+  //mouse
+  sf::Vector2i mousePosView;
+  sf::Vector2f mousePosWindow;
   
 
   void initVariables() {
     this->window = nullptr;
+    // StateEngine stateEngine;
+    
+    int index = 0;
+    for (int i=0; i<20; i++)
+    {
+      for (int j=0; j<20; j++)
+      {
+        this->grid[i][j] = new Tile(i, j);
+      }
+    }
   }
   
   void initWindow() {
-    this->videoMode.height = 500;
-    this->videoMode.width = 1000;
+    this->videoMode.height = 1000;
+    this->videoMode.width = 1500;
     
     this->window = new sf::RenderWindow(this->videoMode, "Pathfinding Visualizer");
+    this->window->setFramerateLimit(60);
   }
   
   void pollEvents() {
@@ -57,16 +103,50 @@ private:
       }
     }
   }
+  
+  void updateMouse() {
+    this->mousePosView = sf::Mouse::getPosition(*this->window);
+    this->mousePosWindow = this->window->mapPixelToCoords(this->mousePosView);
+  }
+  
+  void updateTiles() {
+    for (int i=0; i<20; i++)
+    {
+      for (int j=0; j<20; j++)
+      {
+        Tile *currentTile = this->grid[i][j];
+        Tile::states currentState = currentTile->state;
+        if (currentTile->containsMouse(this->mousePosWindow))
+        {
+          if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+          {
+            if (currentState == Tile::empty || currentState == Tile::hovered) currentTile->setState(Tile::wall);
+          }
+          else if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+          {
+            if (currentState == Tile::wall) currentTile->setState(Tile::hovered);
+          }
+          else
+          {
+            currentTile->setState(Tile::hovered);
+          }
+        }
+        else
+        {
+          if (currentState == Tile::hovered) currentTile->setState(Tile::empty);
+        }
+      }
+    }
+  }
+
+  void renderBoxes() {
+    for (int i=0; i<20; i++)
+    {
+      for (int j=0; j<20; j++)
+      {
+        this->window->draw(this->grid[i][j]->getBox());
+      }
+    }
+  }
 };
 
-class State {
-public:
-
-private:
-};
-
-class StateEngine {
-public:
-
-private:
-};
